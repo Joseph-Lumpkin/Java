8c460213d34885e6ae42e32980ce001dc59b6d9e
Joseph-Lumpkin <nictus1938@gmail.com>
2020-09-24 13:05:13 -0400

    Bug Fixes

--- raspi/src/main/java/com/raspi/controller/LedController.java
+++ raspi/src/main/java/com/raspi/controller/LedController.java
@@ -1,6 +1,7 @@
 package com.raspi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -8,24 +9,25 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
+@RestController
 public class LedController {
 
 	private static GpioPinDigitalOutput pin;
-	
-	@RequestMapping
-	public String greeting() {
-		return "Hello World!";
-	}
-	
-	@RequestMapping("/light")
-	public String light() {
-		if (pin == null) {
-			GpioController gpio = GpioFactory.getInstance();
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "MyLED", PinState.LOW);			
-		}
-		
-		pin.toggle();
-		
-		return "OK";
-	}
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
+    }
+
+    @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
+    }
 }
